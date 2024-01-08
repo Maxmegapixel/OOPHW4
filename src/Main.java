906b923bbdbf5f974dc2c68c6ab3abb8066bad23
Maksim Hancharevich <maxmegapixel@gmail.com>
2024-01-08 19:44:41 +0300

    HW4

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,29 @@
+import MyLinkedList.LinkedContainer;
 import list.MyArrayList;
 import list.MyList;
 
 public class Main {
     public static void main(String[] args) {
-        MyList<Integer> list = new MyArrayList<>();
-        list.add(5);
-        list.add(7);
-        list.add(-1);
-        list.add(2);
-        System.out.println(list);
-        System.out.println(list.size());
-        list.delete(1);
-        System.out.println(list);
-        System.out.println(list.size());
-        System.out.println(list.get(1));
+//        MyList<Integer> list = new MyArrayList<>();
+//        list.add(5);
+//        list.add(7);
+//        list.add(-1);
+//        list.add(2);
+//        System.out.println(list);
+//        System.out.println(list.size());
+//        list.delete(1);
+//        System.out.println(list);
+//        System.out.println(list.size());
+//        System.out.println(list.get(1));
+        LinkedContainer<String> linked = new LinkedContainer<>();
+        linked.addLast("sdfsdf");
+
+        System.out.println(linked.getElementByIndex(0));
+
+
+
+
+
 
     }
 }
